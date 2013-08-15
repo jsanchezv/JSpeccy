1857e0d35496093a32f574f1b2855a572ae38615
Jose Luis Sanchez <zx81@ono.com>
2013-08-15 23:21:24 +0200

    - Use a new method to send sound frames to soundcard

--- src/machine/Audio.java
+++ src/machine/Audio.java
@@ -10,6 +10,7 @@
 package machine;
 
 import configuration.AY8912Type;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.sound.sampled.*;
@@ -19,14 +20,14 @@ class Audio {
     private SourceDataLine line;
     private DataLine.Info infoDataLine;
     private AudioFormat fmt;
-    // Buffer de sonido para el frame actual, hay más espacio del necesario.
-    private final byte[] buf = new byte[4096];
+    // Buffer de sonido para 2 frames a 48 Khz estéreo, hay más espacio del necesario.
+    private final byte[] buf = new byte[8192];
     private final int[] beeper = new int[1024];
     // Buffer de sonido para el AY
     private final int[] ayBufA = new int[1024];
     private final int[] ayBufB = new int[1024];
     private final int[] ayBufC = new int[1024];
-    private int bufp;
+    private int ptrBeeper, ptrBuf;
     private int level;
     private int audiotstates;
     private int samplesPerFrame, frameSize, bufferSize;
@@ -49,7 +50,6 @@ class Audio {
         soundMode = settings.getSoundMode();
         if (soundMode < 0 || soundMode > 3)
             soundMode = 0;
-//              System.out.println("Selected soundMode " +  soundMode);
 
         int channels =  soundMode > 0 ? 2 : 1;
 
@@ -67,14 +67,14 @@ class Audio {
             timeRem = 0;
             samplesPerFrame = samplingFrequency / 50;
             frameSize = samplesPerFrame * 2 * channels;
-//            System.out.println("framesize: " + frameSize);
+
             if (model != spectrumModel) {
                 spectrumModel = model;
                 ay8912.setSpectrumModel(spectrumModel);
             }
 
             step = (long)(((double)spectrumModel.tstatesFrame / (double)samplesPerFrame) * 100000.0);
-            audiotstates = bufp = 0;
+            audiotstates = ptrBeeper = ptrBuf = 0;
             level = 0;
             
             ay8912.setMaxAmplitude(soundMode == 0 ? 8192 : 12288);
@@ -88,14 +88,24 @@ class Audio {
                 default: // Stereo ABC or Mono
                     ay8912.setBufferChannels(ayBufA, ayBufB, ayBufC);
             }
-            ay8912.setAudioFreq(samplingFrequency);
-            ay8912.startPlay();
-            ay = ay8912;
+            
+            if (enabledAY) {
+                ay8912.setAudioFreq(samplingFrequency);
+                ay8912.startPlay();
+                ay = ay8912;
+            }
 
+            /*
+             * La idea de funcionamiento del Audio se basa en tener un buffer de audio de 5 frames
+             * de longitud. Cuando se activa el sonido con el método start, se envía 1 frame
+             * adelantado para iniciar la reproducción e inmediatamente después se envían
+             * paquetes de 2 frames, de modo que la primera vez se envían 3 frames de un golpe.
+             * El frame inicial tienen como misión evitar que el buffer de audio del
+             * sistema se vacíe y se oigan efectos extraños.
+             */
+            bufferSize = frameSize * 2;
             try {
-                line.open(fmt);
-                bufferSize = line.getBufferSize();
-                line.start();
+                line.open(fmt, frameSize * 5);
             } catch (LineUnavailableException ex) {
                 Logger.getLogger(Audio.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -126,7 +136,7 @@ class Audio {
                     if (value != 0) {
                         level += ((float) diff / (float) step) * value;
                     }
-                    beeper[bufp++] = (int) level;
+                    beeper[ptrBeeper++] = (int) level;
                 } else {
                     // el tiempo transcurrido no basta para completar la muestra
                     timeRem += time;
@@ -139,7 +149,7 @@ class Audio {
 
             // se añaden muestras completas mientras se pueda
             while (time >= step) {
-                beeper[bufp++] = value;
+                beeper[ptrBeeper++] = value;
                 time -= step;
             }
         }
@@ -154,27 +164,8 @@ class Audio {
         }
     }
 
-    private void flushBuffer(int len) {
-
-        if (line != null) {
-
-            synchronized (buf) {
-                line.write(buf, 0, len);
-                // Deberían haber siempre 2 frames de sonido adelantados. Si no los hay
-                // rellena en previsión de que falten. Es más molesto el efecto del buffer
-                // vacío que la repetición de un frame.
-                if (available() > bufferSize - (frameSize << 1)) {
-                    System.out.println("UNDERRUN!");
-                    line.write(buf, 0, len);
-                    line.write(buf, 0, len);
-                    bufp = 0;
-                }
-            }
-        }
-    }
-
     synchronized public void flush() {
-        level = bufp = 0;
+        level = ptrBeeper = 0;
         timeRem = 0;
         if (line != null)
             line.flush();
@@ -182,32 +173,54 @@ class Audio {
 
     synchronized public void endFrame() {
 
-        if (bufp == 0)
+        if (ptrBeeper == 0)
             return;
-
-        int ptr = 0;
+                
+//        System.out.println(bufp + ", " + ay.getSampleCount());
         
         if (soundMode == 0) {
-            ptr = endFrameMono(bufp);
+            endFrameMono(ptrBeeper);
         }
         else {
-            ptr = endFrameStereo(bufp);
+            endFrameStereo(ptrBeeper);
         }
 
-        flushBuffer(ptr);
+        if (ptrBuf == bufferSize) {
+            // La línea puede no estar en estado running bien porque aún
+            // no se ha hecho el start, bien porque se haya vaciado de datos.
+            // Si hace falta se pone en marcha y en cualquier caso, se prerrellena
+            // con un frame de audio antes de enviar los dos que corresponden.
+            if (!line.isRunning()) {
+                if (!line.isActive())
+                    line.start();
+                line.write(buf, 0, frameSize);
+            }
+//            int available = line.available();
+//            System.out.println(available);
+            // Si hay espacio para un paquete de 2 frames, se envía.
+            // En caso contrario, nos lo saltamos porque es señal de que
+            // el buffer tá petao.
+            if (line.available() >= bufferSize) {
+                line.write(buf, 0, bufferSize);
+                
+            } else {
+//                line.write(buf, 0, available);
+                System.out.println("OVERRUN!");
+            }
+            ptrBuf = 0;
+        }
+        
         
         if (enabledAY) {
             ay.endFrame();
         }
         
-        bufp = 0;
+        ptrBeeper = 0;
 
         audiotstates -= spectrumModel.tstatesFrame;
     }
 
-    private int endFrameMono(int nSamples) {
-
-        int ptr = 0;
+    private void endFrameMono(int nSamples) {
 
         // El código está repetido, lo que es correcto. Si no se hace así habría
         // que meter la comprobación de enabledAY dentro del bucle, lo que
@@ -215,21 +228,19 @@ class Audio {
         if (enabledAY) {
             for (int idx = 0; idx < nSamples; idx++) {
                 int sample = beeper[idx] + ayBufA[idx] + ayBufB[idx] + ayBufC[idx];
-                buf[ptr++] = (byte) sample;
-                buf[ptr++] = (byte)(sample >>> 8);
+                buf[ptrBuf++] = (byte) sample;
+                buf[ptrBuf++] = (byte)(sample >>> 8);
             }
         } else {
             for (int idx = 0; idx < nSamples; idx++) {
-                buf[ptr++] = (byte) beeper[idx];
-                buf[ptr++] = (byte) (beeper[idx] >>> 8);
+                buf[ptrBuf++] = (byte) beeper[idx];
+                buf[ptrBuf++] = (byte) (beeper[idx] >>> 8);
             }
         }
-        return ptr;
     }
 
-    private int endFrameStereo(int nSamples) {
+    private void endFrameStereo(int nSamples) {
 
-        int ptr = 0;
 
         // El código está repetido, lo que es correcto. Si no se hace así habría
         // que meter la comprobación de enabledAY dentro del bucle, lo que
@@ -242,42 +253,28 @@ class Audio {
                 sampleL = beeper[idx] + ayBufA[idx] + center + side;
                 side = (int)(ayBufA[idx] * 0.3);
                 sampleR = beeper[idx] + side + center + ayBufC[idx];
-                buf[ptr++] = (byte) sampleL;
-                buf[ptr++] = (byte)(sampleL >>> 8);
-                buf[ptr++] = (byte) sampleR;
-                buf[ptr++] = (byte)(sampleR >>> 8);
+                buf[ptrBuf++] = (byte) sampleL;
+                buf[ptrBuf++] = (byte)(sampleL >>> 8);
+                buf[ptrBuf++] = (byte) sampleR;
+                buf[ptrBuf++] = (byte)(sampleR >>> 8);
             }
         } else {
             byte lsb, msb;
             for (int idx = 0; idx < nSamples; idx++) {
                 lsb = (byte) beeper[idx];
                 msb = (byte) (beeper[idx] >>> 8);
-                buf[ptr++] = lsb;
-                buf[ptr++] = msb;
-                buf[ptr++] = lsb;
-                buf[ptr++] = msb;
+                buf[ptrBuf++] = lsb;
+                buf[ptrBuf++] = msb;
+                buf[ptrBuf++] = lsb;
+                buf[ptrBuf++] = msb;
             }
         }
-        return ptr;
     }
 
     public void reset() {
         audiotstates = 0;
-        level = bufp = 0;
+        level = ptrBeeper = 0;
         timeRem = 0;
-        java.util.Arrays.fill(beeper, 0);
-    }
-    
-    // El método available() de SourceDataLine es un caos. En Linux devuelve bufferSize + el
-    // número de bytes que tiene disponibles. En Windows y OSX, de manera más lógica,
-    // devuelve el número de bytes que le quedan libres en el buffer.
-    // Huele sospechosamente a bug en la parte específica de Linux (con PulseAudio).
-    public int available() {
-        int available = line.available();
-        if (available > bufferSize)
-            available -= bufferSize;
-        
-//        System.out.println(available);
-        return available;
+        Arrays.fill(beeper, 0);
     }
 }
