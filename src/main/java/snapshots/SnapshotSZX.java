a3927a8321733dd4f689560a45dd32818f707177
Jose Luis Sanchez <jspeccy@gmail.com>
2021-12-08 20:40:52 +0100

    Delete unused imports

--- src/main/java/snapshots/SnapshotSZX.java
+++ src/main/java/snapshots/SnapshotSZX.java
@@ -14,12 +14,9 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterInputStream;
 
