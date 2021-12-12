61a856c5c404f784a2ad20bc9e176afddc7021f7
Jose Luis Sanchez <jspeccy@gmail.com>
2021-12-12 17:58:33 +0100

    Do final field

--- src/main/java/gui/CommandLineOptions.java
+++ src/main/java/gui/CommandLineOptions.java
@@ -32,55 +32,55 @@ public class CommandLineOptions {
     @Option(name = "-m", aliases = "--model", metaVar = "CommandLineOptions.metaVar.model.text",
             usage = "CommandLineOptions.model.text")
     private Model model = Model.sp48k;
-    
+
     @Option(name = "-u", aliases = "--ulaplus", usage = "CommandLineOptions.ulaplus.text")
     private boolean ulaplus;
-    
+
     @Option(name = "-if1", aliases = "--interface1", usage = "CommandLineOptions.interface1.text")
     private boolean if1;
-    
+
     @Option(name = "--microdrive-file", depends = "-if1", metaVar = "CommandLineOptions.metaVar.file.text",
             usage = "CommandLineOptions.microdriveFile.text")
     private File if1mdv;
-    
+
     @Option(name = "--multiface", usage = "CommandLineOptions.multiface.text")
     private boolean multiface;
-    
+
     @Option(name = "--mf128-on-48k", depends = "--multiface" , usage = "CommandLineOptions.mf128on48k.text")
     private boolean mf128on48k;
-    
+
     @Option(name = "--lec", usage = "CommandLineOptions.lec.text")
     private boolean lec;
-    
+
     @Option(name = "--emulate-128k-bug", usage = "CommandLineOptions.emulate128kBug.text")
     private boolean bug128k;
-    
+
     @Option(name = "--issue2", usage = "CommandLineOptions.issue2.text")
     private boolean issue2;
 
     @Option(name = "-j", aliases = "--joystick", metaVar = "CommandLineOptions.metaVar.model.text",
             usage = "CommandLineOptions.joystick.text")
     private JoystickModel joystick = JoystickModel.NONE;
-    
+
     @Option(name = "--map-pc-keyboard", usage = "CommandLineOptions.mapPCkeyboard.text")
     private boolean mapPCkeys;
-    
+
     @Option(name = "--recreated-zx", usage = "CommandLineOptions.recreatedZX.text")
     private boolean recreatedZX;
 
     @Option(name = "-z", aliases = "--zoom", metaVar = "CommandLineOptions.metaVar.size.text",
             usage = "CommandLineOptions.zoom.text")
     private int zoom = 1;
-    
+
     @Option(name = "--scanlines", usage = "CommandLineOptions.scanlines.text")
     private boolean scanlines;
-    
+
     @Option(name = "--mute", usage = "CommandLineOptions.mute.text")
     private boolean silence;
 
     @Option(name = "--melodik", usage = "CommandLineOptions.melodik.text")
     private boolean ayEnabled;
-    
+
     @Option(name = "--hifi-sound", usage = "CommandLineOptions.hifi.text")
     private boolean hifi;
 
@@ -88,38 +88,38 @@ public class CommandLineOptions {
     @Option(name = "--sound-mode", metaVar = "CommandLineOptions.metaVar.mode.text",
             usage = "CommandLineOptions.soundMode.text")
     private SoundMode soundMode = SoundMode.MONO;
-    
+
     enum ZoomFilter { STANDARD, BILINEAL, BICUBIC };
     @Option(name = "--zoom-filter", metaVar = "CommandLineOptions.metaVar.filter.text",
             usage = "CommandLineOptions.zoomFilter.text")
     private ZoomFilter zoomFilter = ZoomFilter.STANDARD;
-    
+
     enum BorderSize { NONE, STANDARD, FULL, HUGE };
     @Option(name = "--border-size", metaVar = "CommandLineOptions.metaVar.size.text",
             usage = "CommandLineOptions.borderSize.text")
     private BorderSize borderSize = BorderSize.STANDARD;
-    
+
     @Option(name = "--no-load-trap", usage = "CommandLineOptions.noLoadTrap.text")
     private boolean loadTrap;
-    
+
     @Option(name = "--fastload", usage = "CommandLineOptions.fastload.text")
     private boolean fastload;
-    
+
     @Option(name = "--no-accelerated-loading", usage = "CommandLineOptions.noAcceleratedLoading.text")
     private boolean acceleratedLoading;
-    
+
     @Option(name = "--no-autoload", usage = "CommandLineOptions.noAutoLoad.text")
     private boolean autoload;
-    
+
     @Option(name = "--no-save-trap", usage = "CommandLineOptions.noSaveTrap.text")
     private boolean saveTrap;
-    
+
     @Option(name = "--no-confirm-actions", usage = "CommandLineOptions.noConfirmActions.text")
     private boolean confirmActions;
 
     // receives other command line parameters than options
     @Argument
-    private List<String> arguments = new ArrayList<>();
+    private final List<String> arguments = new ArrayList<>();
 
     /**
      * @return the argumentsissue2
@@ -127,7 +127,7 @@ public class CommandLineOptions {
     public List<String> getArguments() {
         return arguments;
     }
-    
+
     /**
      * @return the printUsage
      */
@@ -148,7 +148,7 @@ public class CommandLineOptions {
     public File getIf1mdv() {
         return if1mdv;
     }
-    
+
     public void copyArgumentsToSettings() {
         // hardware options
         settings.getSpectrumSettings().setDefaultModel(model.ordinal());
@@ -177,7 +177,7 @@ public class CommandLineOptions {
         settings.getSpectrumSettings().setScanLines(scanlines);
         settings.getSpectrumSettings().setZoomMethod(zoomFilter.ordinal());
         settings.getSpectrumSettings().setBorderSize(borderSize.ordinal());
-        
+
         // sound options
         settings.getSpectrumSettings().setMutedSound(silence);
         if (!silence) {
@@ -185,14 +185,14 @@ public class CommandLineOptions {
             settings.getSpectrumSettings().setHifiSound(hifi);
             settings.getAY8912Settings().setSoundMode(soundMode.ordinal());
         }
-        
+
         // Tape Options
         settings.getTapeSettings().setEnableLoadTraps(!loadTrap);
         settings.getTapeSettings().setFlashLoad(fastload);
         settings.getTapeSettings().setAccelerateLoading(!acceleratedLoading);
         settings.getTapeSettings().setAutoLoadTape(!autoload);
         settings.getTapeSettings().setEnableSaveTraps(!saveTrap);
-        
+
         // Emulator options
         settings.getEmulatorSettings().setConfirmActions(!confirmActions);
     }
